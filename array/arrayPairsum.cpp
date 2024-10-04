1c4bbd3ea9be4cf8e5def03e48d625d1b620509d
Cpp-Basics <akashkumar9399382@gmail.com>
2024-10-04 20:40:11 +0530

    count totalpair

--- array/arrayPairsum.cpp
+++ array/arrayPairsum.cpp
@@ -4,14 +4,17 @@ using namespace std;
 int main(){
     int arr[] = {1,2,3,4,5,6,7,8,9,0};
     int s = sizeof(arr)/4;
+    int totalpair = 0;
     int x = 9;
     for (int i = 0; i <s;i++){
-        for (int j = 0; j < s;j++){
+        for (int j =0; j < s;j++){     //  for (int j = i+1 , j<s ,j++) it will remove duplicate or reverse pair from output.
             if((arr[i]+arr[j]==x)){
+                totalpair++;
                 cout << "(" << i << "," << j << ")";
             }
             else 
                 continue;
         }
     }
+    cout << "\nNumber of pairs : " << totalpair;
 }
