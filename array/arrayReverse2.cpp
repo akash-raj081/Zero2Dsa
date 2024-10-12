7c1981bf6b6ab50e5ed449e722f6e318c7d11123
Cpp-Basics <akashkumar9399382@gmail.com>
2024-10-12 15:42:46 +0530

    code to rotate array's selected range

--- array/arrayReverse2.cpp
+++ array/arrayReverse2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void reverse(int s ,int arr[] , int si , int ei ){
+void reverse(int arr[] , int si , int ei ){
     for (int i = si, j = ei; i < j;i++,j--){
         int temp = arr[i];
         arr[i] = arr[j];
@@ -11,12 +11,15 @@ void reverse(int s ,int arr[] , int si , int ei ){
 int main(){
     int arr[8] = {4, 5, 6, 78, 9, 0, 4, 8};
     int s = sizeof(arr) / 4;
-    int Rt = 67;
+    int Rt;
+    cout << "Enter number of rotation : " << endl;
+    cin >> Rt;
+    
     Rt = Rt % s;    
 
-    reverse(s,arr,0,s-1);
-    reverse(s,arr, 0, Rt - 1);
-    reverse(s, arr, Rt, s - 1);
+    reverse(arr,0,s-1);
+    reverse(arr, 0, Rt - 1);
+    reverse(arr, Rt, s - 1);
 
     cout << "after Reverse array is :" << endl;
     for (int i = 0; i < s;i++){
