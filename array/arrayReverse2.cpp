9d226d1e299adbd030be21b8a70892fad5412639
Cpp-Basics <akashkumar9399382@gmail.com>
2024-10-14 00:37:58 +0530

    rotate user array

--- array/arrayReverse2.cpp
+++ array/arrayReverse2.cpp
@@ -9,19 +9,30 @@ void reverse(int arr[] , int si , int ei ){
     return ;
 }
 int main(){
-    int arr[8] = {4, 5, 6, 78, 9, 0, 4, 8};
-    int s = sizeof(arr) / 4;
+    int arr[8];
+    int s;
+    cout << "Enter size of the array "<<endl;
+    cin >> s;
+    cout << "Enter array elemets :" << endl;
+    for (int i = 0; i < s;i++){
+        cin>>arr[i];
+    }
+    // int s = sizeof(arr) / 4;
     int Rt;
     cout << "Enter number of rotation : " << endl;
     cin >> Rt;
     
-    Rt = Rt % s;    
+    Rt = Rt % s;
+    cout << "Before Rotating array is : " << endl;
+    for (int i = 0; i <s;i++){
+        cout << arr[i] << " ";
+    }
 
     reverse(arr,0,s-1);
     reverse(arr, 0, Rt - 1);
     reverse(arr, Rt, s - 1);
 
-    cout << "after Reverse array is :" << endl;
+    cout << "\nafter Reverse array is :" << endl;
     for (int i = 0; i < s;i++){
         cout << arr[i] << " ";
     }
